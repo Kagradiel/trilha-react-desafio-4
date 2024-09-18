e7d81ebd4d3b45104df6298ff831d5617fb58ffa
Kagradiel <matheusp_torres@hotmail.com>
2024-09-18 15:12:58 -0300

    desafio concluido

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
+export const ButtonContainer = styled.button<{ disabled: boolean }>`
+  width: 100%;
+  height: 42px;
+  background-color: #81259d;
+  opacity: ${(props) => (props.disabled ? "1" : "0.6")};
+  color: #fff;
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  border: 1px solid #81259d;
+  border-radius: 21px;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+    opacity: 0.6;
+    cursor: ${(props) => (props.disabled ? "pointer" : "not-allowed")};
+  }
+`;
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,5 @@
 export interface IButtonProps {
-    title: string;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  title: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled: boolean;
 }
